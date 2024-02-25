b7a439287508f936967b161f910e027a0b4eb276
Slava Yakimov <fintechmustreets@gmail.com>
2024-02-25 17:18:34 +0500

    inintial: added svg icons

---
+++ src/assets/index.tsx
@@ -0,0 +1,55 @@
+import Apple from './Apple';
+import Azure from './Azure';
+import Bitbucket from './Bitbucket';
+import Discord from './Discord';
+import Facebook from './Facebook';
+import Figma from './Figma';
+import GitHub from './GitHub';
+import Gitlab from './Gitlab';
+import Google from './Google';
+import Kakao from './Kokao';
+import Keycloak from './Keycloak';
+import LinkedIn from './LinkedIn';
+import Notion from './Notion';
+import Slack from './Slack';
+import Spotify from './Spotify';
+import Tinkoff from './Tinkoff';
+import Twitch from './Twitch';
+import Twitter from './Twitter';
+import Vkontakte from './Vkontakte';
+import WorkOS from './WorkOS';
+import Yandex from './Yandex';
+import Zoom from './Zoom';
+import Email from './Email';
+import Phone from './Phone';
+import type { iAssets } from '../data';
+
+export type iconsAssets=Record<iAssets,iAssets>
+
+export {
+    Apple,
+    Azure,
+    Bitbucket,
+    Discord,
+    Email,
+    Facebook,
+    Figma,
+    GitHub,
+    Gitlab,
+    Google,
+    Kakao,
+    Keycloak,
+    LinkedIn,
+    Notion,
+    Phone,
+    Slack,
+    Spotify,
+    Tinkoff,
+    Twitch,
+    Twitter,
+    Vkontakte,
+    WorkOS,
+    Yandex,
+    Zoom,
+};
+
